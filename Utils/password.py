bbec26177c290c88dfe649344f03b9d6e7bcea0e
Zerimar10 <tartarushell@hotmail.com>
2025-11-21 11:46:09 -0700

    Delete Utils directory

--- Utils/password.py
+++
@@ -1,6 +0,0 @@
-# ==============================================
-# Archivo: Utils/password.py
-# Contiene la contraseña para el tab Almacén
-# ==============================================
-
-ALMACEN_PASSWORD = "nordson2025"
