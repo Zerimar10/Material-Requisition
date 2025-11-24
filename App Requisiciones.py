23ff9d606cfdd517c47a60deb07de7f4e3b9e510
Zerimar10 <tartarushell@hotmail.com>
2025-11-24 12:17:17 -0700

    Update App Requisiciones.py

--- App Requisiciones.py
+++ App Requisiciones.py
@@ -293,6 +293,8 @@ with tab1:
         st.session_state.msg_ok = True
         st.session_state.reset_form = True
 
+        st.rerun()
+
 # ============================================================
 # TAB 2 — PANEL DE ALMACÉN
 # ============================================================
@@ -510,6 +512,7 @@ with tab2:
             mime="text/csv",
             use_container_width=True
         )
+
 
 
 
